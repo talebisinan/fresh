0d56d1f1dcd4d0a961c662f1fae032e0cd64e269
Sinan Talebi <sinan.talebi@workgenius.com>
2022-09-27 16:05:11 +0200

    Initial commit

---
+++ routes/search.tsx
@@ -0,0 +1,41 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+
+const NAMES = ["Alice", "Bob", "Charlie", "Dave", "Eve", "Frank"];
+
+interface Data {
+  results: string[];
+  query: string;
+}
+
+export const handler: Handlers<Data> = {
+  GET(req, ctx) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get("q") || "";
+    const results = NAMES.filter((name) => name.includes(query));
+    return ctx.render({ results, query });
+  },
+};
+
+export default function Page({ data }: PageProps<Data>) {
+  const { results, query } = data;
+  return (
+    <div className="grid gap-4">
+      <form className="$1 grid gap-4">
+        <input
+          type="text"
+          name="q"
+          value={query}
+          className="p-2 border focus:border-blue-200"
+        />
+        <button type="submit" className="border bg-gray-100 p-2">
+          Search
+        </button>
+      </form>
+      <ul>
+        {results.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
